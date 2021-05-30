4976007116acf18f3c8bcf031269edd93ab4ed5a
shubhamkurkure <shubhamkurkure@gmail.com>
2021-05-30 15:59:01 -0400

    Updates

---
+++ precache-manifest.c51bea4f0a08d6c05b4d1d7c32eac67a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4d1873307fc5441747116b45fd2d6d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "26a312e03d01993e685b",
+    "url": "/portfolio/static/css/3.ed673b97.chunk.css"
+  },
+  {
+    "revision": "86fb9f9ceee7066edb9e",
+    "url": "/portfolio/static/css/4.0110b6a7.chunk.css"
+  },
+  {
+    "revision": "7f055d6fb221cd533b0f",
+    "url": "/portfolio/static/css/main.0e5c270e.chunk.css"
+  },
+  {
+    "revision": "2e950a08afd58d55298a",
+    "url": "/portfolio/static/js/2.65e43be7.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/portfolio/static/js/2.65e43be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26a312e03d01993e685b",
+    "url": "/portfolio/static/js/3.9a5e9046.chunk.js"
+  },
+  {
+    "revision": "86fb9f9ceee7066edb9e",
+    "url": "/portfolio/static/js/4.206b35cf.chunk.js"
+  },
+  {
+    "revision": "7f055d6fb221cd533b0f",
+    "url": "/portfolio/static/js/main.f0c44da5.chunk.js"
+  },
+  {
+    "revision": "3b0c2bf75f93c853b41f",
+    "url": "/portfolio/static/js/runtime-main.421d5a39.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/portfolio/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "9f48333a03fd986c5a62d6a051959209",
+    "url": "/portfolio/static/media/JP-Morgan-Chase-Logo.9f48333a.png"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/portfolio/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "33911318e7cf2ded7d7f46a3531780a8",
+    "url": "/portfolio/static/media/coveyTown.33911318.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "f18c61bc6fc476c5a155cbe1b07d1167",
+    "url": "/portfolio/static/media/northeasternLogo.f18c61bc.png"
+  },
+  {
+    "revision": "e0b3936f0ed4a30037105030ddc0f3d7",
+    "url": "/portfolio/static/media/propertyListingLogo.e0b3936f.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "a0f28f0fa6e8de8b7dc9bd60587aa885",
+    "url": "/portfolio/static/media/stateStreetLogo.a0f28f0f.jpeg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "f3ddfb229f7afc60703e05c38d4f8cfe",
+    "url": "/portfolio/static/media/tifrLogo.f3ddfb22.png"
+  },
+  {
+    "revision": "c567986e1b384faa11f6e0d6f5f093e4",
+    "url": "/portfolio/static/media/universiyOfMumbai.c567986e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cda608253d5136e83e02978b49900706.js"
+  "/portfolio/precache-manifest.c51bea4f0a08d6c05b4d1d7c32eac67a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
