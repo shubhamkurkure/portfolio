588b0a05866b8b0216100d408e730fae00ad43e1
shubhamkurkure <shubhamkurkure@gmail.com>
2021-05-30 15:56:52 -0400

    Updates

---
+++ precache-manifest.cda608253d5136e83e02978b49900706.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7087229a0400220d0725d52f56526cb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26a312e03d01993e685b",
+    "url": "/static/css/3.ed673b97.chunk.css"
+  },
+  {
+    "revision": "86fb9f9ceee7066edb9e",
+    "url": "/static/css/4.0110b6a7.chunk.css"
+  },
+  {
+    "revision": "fedbedb7ee5af5983740",
+    "url": "/static/css/main.0690334e.chunk.css"
+  },
+  {
+    "revision": "2e950a08afd58d55298a",
+    "url": "/static/js/2.65e43be7.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.65e43be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26a312e03d01993e685b",
+    "url": "/static/js/3.9a5e9046.chunk.js"
+  },
+  {
+    "revision": "86fb9f9ceee7066edb9e",
+    "url": "/static/js/4.206b35cf.chunk.js"
+  },
+  {
+    "revision": "fedbedb7ee5af5983740",
+    "url": "/static/js/main.76904b8d.chunk.js"
+  },
+  {
+    "revision": "24ac6db9b676f32d4190",
+    "url": "/static/js/runtime-main.93a1ea0a.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "9f48333a03fd986c5a62d6a051959209",
+    "url": "/static/media/JP-Morgan-Chase-Logo.9f48333a.png"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "33911318e7cf2ded7d7f46a3531780a8",
+    "url": "/static/media/coveyTown.33911318.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "f18c61bc6fc476c5a155cbe1b07d1167",
+    "url": "/static/media/northeasternLogo.f18c61bc.png"
+  },
+  {
+    "revision": "e0b3936f0ed4a30037105030ddc0f3d7",
+    "url": "/static/media/propertyListingLogo.e0b3936f.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "a0f28f0fa6e8de8b7dc9bd60587aa885",
+    "url": "/static/media/stateStreetLogo.a0f28f0f.jpeg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "f3ddfb229f7afc60703e05c38d4f8cfe",
+    "url": "/static/media/tifrLogo.f3ddfb22.png"
+  },
+  {
+    "revision": "c567986e1b384faa11f6e0d6f5f093e4",
+    "url": "/static/media/universiyOfMumbai.c567986e.png"
+  }
+]);
